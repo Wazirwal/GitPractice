5ebf24b46459dab3520dc4f44f9818d6eb0d9f32
gitpackage <noor@gmail.com>
2023-02-21 21:26:15 -0500

    this is the third change

--- src/GitPractice/GitPractice2nd.java
+++ src/GitPractice/GitPractice2nd.java
@@ -1,5 +1,25 @@
 package GitPractice;
 
 public class GitPractice2nd {
+	
+	
+	
+	 private void Sysout() {
+		
+
+
+  int x = 10;
+  int y = 5;
+  System.out.println(x+y);
+
+ 
+	
 
 }
+
+	 public void test() {
+		 System.out.println("This is from the local Repo");
+	 }
+
+}
+
